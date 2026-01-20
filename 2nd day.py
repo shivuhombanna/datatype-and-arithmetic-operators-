ae6455ced76e6ac510f895330258b72e9813c315
Shivaraj H <140573544+shivuhombanna@users.noreply.github.com>
2026-01-20 18:49:45 +0530

    Add files via upload

---
+++ 2nd day.py
@@ -0,0 +1,37 @@
+#data types
+age=10
+name="shivu"
+is_student = False
+weight=55.8
+print(type(name))
+
+age_float = int(age)
+print(age_float)
+
+# type convertion
+a="20"
+print(int(a)+age)
+
+
+
+
+# swap 2 charecter without using 3 value
+x=10
+y=30
+x,y=y,x
+print(x)
+print(y)
+
+
+#arithametic operators 
+n=20
+m=8
+print(n+m)
+print(n-m)
+print(n/m)
+print(n*m)
+print(n//m)
+print(n%m)
+print(n**m)
+print(n+m-m*6)
+
