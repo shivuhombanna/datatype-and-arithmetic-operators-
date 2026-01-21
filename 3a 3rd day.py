a3ba57f9e43b7264bc0ac0c011132de14dc85a76
Shivaraj H <140573544+shivuhombanna@users.noreply.github.com>
2026-01-21 09:20:59 +0530

    Add files via upload

---
+++ 3a 3rd day.py
@@ -0,0 +1,7 @@
+boy_nam=input("boy ")
+b_age=int(input("boy age  "))
+girl_nam=input("girl ")
+g_age=int(input("girl age  "))
+age_diff=b_age-g_age
+print(boy_nam +" loves is "+ girl_nam +" ,age difference is "+ str(age_diff) )
+print(f"{boy_nam} loves {girl_nam} ,age difference is  {age_diff} ")
