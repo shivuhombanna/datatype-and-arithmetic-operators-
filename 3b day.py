fdf30329c2a79702f52d27c6ea16b020551de00c
Shivaraj H <140573544+shivuhombanna@users.noreply.github.com>
2026-01-22 21:20:25 +0530

    Add files via upload

---
+++ 3b day.py
@@ -0,0 +1,34 @@
+#concatination
+first_name="Shivu"
+last_name="hombanna"
+full_name= first_name +" "+ last_name
+print(full_name)
+
+#Repitation
+mesage="this is  Worning ! "
+print(mesage*5)
+
+#string methods
+print(mesage.upper())
+print(mesage.lower())
+print(mesage.strip()*2)
+print(mesage.replace(" Worning" ,"right"))
+
+print(len(mesage))#lenth of mesage
+
+#write next line using 3 quates 
+a='''shivu sead "hello "
+   this is shivu sed good bay'''
+print(a)
+
+name="shivaraj"
+print(name[2])
+print(name[2:7])
+print(name[:5])
+print(name[-4:])
+print( name[::3])
+
+
+#escape sequence 
+b="hii my \t name \n is shivu"
+print(b)
