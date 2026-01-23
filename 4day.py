c6952fd3bcdbc43dffe78c943e1c6f689bc5ac6c
Shivaraj H <140573544+shivuhombanna@users.noreply.github.com>
2026-01-23 20:38:37 +0530

    Add files via upload

---
+++ 4day.py
@@ -0,0 +1,13 @@
+#operators 
+a=10
+a+=100#a=a+100 write in small assingment operation 
+print(a)
+
+x=30
+y=66
+print(x>=y)#comparison
+
+#member operators
+s="shivaraj"
+s1="shivarajm"
+print(not("a" in s) )
