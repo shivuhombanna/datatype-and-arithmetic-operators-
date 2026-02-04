c0dedaf90e5287e462abc4b74ed68ed76ca833d3
Shivaraj H <140573544+shivuhombanna@users.noreply.github.com>
2026-02-04 20:20:08 +0530

    Add files via upload

---
+++ 11th day.py
@@ -0,0 +1,13 @@
+# student=["shivi","karthik ","kiran"]
+# marks=[25,80,67]
+
+# student_marks={}
+
+# for i in range(len(student)):
+#     student_marks[student[i] ]= marks[i]
+# print(student_marks)
+
+print("list of inputs ")
+
+l=[int(num)  for num in input("enter the sting").split() ]
+print(l)
