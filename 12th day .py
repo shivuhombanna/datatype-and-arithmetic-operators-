f30d093052149c36eb0e4595b66db1018dd78eae
Shivaraj H <140573544+shivuhombanna@users.noreply.github.com>
2026-02-05 20:49:21 +0530

    Add files via upload

---
+++ 12th day .py
@@ -0,0 +1,17 @@
+#greet function
+def greet():
+    print("hello word ")
+greet()
+
+#2nd 
+def greet_name(name):
+    print(f"my is {name}")
+# user_name="shivu"
+greet_name(name="hombanna")
+user_name2="karthik"
+greet_name(user_name2)
+
+#add a 2 numbers
+def add_2num(a,b):
+    print(f"{a+b}")
+add_2num(6,5)
