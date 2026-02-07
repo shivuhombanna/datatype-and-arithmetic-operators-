4ea43b362b153a15b447c040c4e6622321bd2cd5
Shivaraj H <140573544+shivuhombanna@users.noreply.github.com>
2026-02-07 20:05:47 +0530

    Add files via upload

---
+++ 13th day.py
@@ -0,0 +1,17 @@
+# #variabal lenth argumant 
+# def total_sum(*numbers):
+#     total=0
+#     for num in numbers:
+#         total +=num
+#     return total
+# print(total_sum(1,9,8))
+
+# #dicnary functon 
+# def stu_details(**detaile):
+#     for key,value in detaile.items():
+#         print(f"{key}:{value}")
+# (stu_details(name="shivu", college="drr",age=21 ))
+
+#lambda
+double=lambda x:x*8
+print(double(67))
